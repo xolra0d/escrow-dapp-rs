751a028d472c11be71b234bbfc4f56afd7716613
xolra0d <sn24008@edu.lu.lv>
2025-08-02 16:56:20 +0300

    updated logic

--- programs/main/src/lib.rs
+++ programs/main/src/lib.rs
@@ -8,70 +8,81 @@ declare_id!("A6kupDWJAzffm1J3cKVNRBLL3zuNT7cXFFgeMxDPZ3pc");
 pub mod main {
     use super::*;
 
-    pub fn initialize_escrow(ctx: Context<InitializeEscrow>, amount: u64) -> Result<()> {
-        let sol_vault = &mut ctx.accounts.sol_vault;
+    pub fn initialize_escrow(ctx: Context<InitializeEscrow>, deal_info: DealInfo) -> Result<()> {
+        if deal_info.title.len() > 128 {
+            return err!(ErrorCode::TitleTooLong)
+        }
+        let escrow_account = &mut ctx.accounts.escrow_account;
         let signer = &mut ctx.accounts.signer;
         let system_program = &ctx.accounts.system_program;
 
-        let vault_balance_before = sol_vault.get_lamports();
+        let vault_balance_before = escrow_account.get_lamports();
 
-        transfer(
-           CpiContext::new(
-                system_program.to_account_info(),
-                Transfer {
-                    from: signer.to_account_info(),
-                    to: sol_vault.to_account_info(),
-                },
-           ),
-           amount,
-        )?;
-
-        let vault_balance_after = sol_vault.get_lamports();
-
-        require_eq!(vault_balance_before + amount, vault_balance_after);
-
-        Ok(())
-    }
-
-    pub fn confirm_delivery(ctx: Context<ConfirmDelivery>, amount: u64) -> Result<()> {
-        let sol_vault = &mut ctx.accounts.sol_vault;
-        let _signer = &mut ctx.accounts.signer;
-        let recipient  = &mut ctx.accounts.recipient;
-        let system_program = &ctx.accounts.system_program;
-
-        let vault_balance_before = sol_vault.get_lamports();
-
-        let bump = &[ctx.bumps.sol_vault];
-        let seeds = &[b"vault".as_ref(), bump];
-        let signer_seeds = &[&seeds[..]];
+        escrow_account.title = deal_info.title;
+        escrow_account.seller = deal_info.seller;
+        escrow_account.buyer = deal_info.buyer;
+        escrow_account.bump = ctx.bumps.escrow_account;
+        escrow_account.lamports = deal_info.lamports;
 
         transfer(
             CpiContext::new(
                 system_program.to_account_info(),
                 Transfer {
-                    from: sol_vault.to_account_info(),
-                    to: recipient.to_account_info(),
+                    from: signer.to_account_info(),
+                    to: escrow_account.to_account_info(),
                 },
-            ).with_signer(signer_seeds),
-            amount,
+           ),
+            deal_info.lamports,
         )?;
 
-        let vault_balance_after = sol_vault.get_lamports();
+        let vault_balance_after = escrow_account.get_lamports();
 
-        require_eq!(vault_balance_before - amount, vault_balance_after);
+        require_eq!(vault_balance_before + deal_info.lamports, vault_balance_after);
+
+        Ok(())
+    }
+
+    pub fn confirm_delivery(ctx: Context<ConfirmDelivery>) -> Result<()> {
+        let escrow_account = &mut ctx.accounts.escrow_account;
+        let recipient  = &mut ctx.accounts.recipient;
+
+        let amount = escrow_account.lamports;
+
+        **escrow_account.to_account_info().try_borrow_mut_lamports()? -= amount;
+        **recipient.to_account_info().try_borrow_mut_lamports()? += amount;
+
 
         Ok(())
     }
 }
 
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct DealInfo {
+    title: String, // max 128 chars
+    lamports: u64,
+    seller: Pubkey,
+    buyer: Pubkey,
+}
+
+#[account]
+pub struct EscrowAccount {
+    pub title: String,
+    pub seller: Pubkey,
+    pub buyer: Pubkey,
+    pub lamports: u64,
+    pub bump: u8,
+}
+
 #[derive(Accounts)]
 pub struct InitializeEscrow<'info> {
     #[account(
-        mut,
-        seeds=[b"vault".as_ref()],
+        init_if_needed,
+        payer = signer,
+        seeds = [b"vault".as_ref()],
         bump,
+        space = 8 + (4 + 128) + 8 + 32 + 32 + 8 + 1,
     )]
-    pub sol_vault: SystemAccount<'info>,
+    pub escrow_account: Account<'info, EscrowAccount>,
     #[account(mut)]
     pub signer: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -82,12 +93,19 @@ pub struct ConfirmDelivery<'info> {
     #[account(
         mut,
         seeds=[b"vault".as_ref()],
-        bump
+        bump = escrow_account.bump,
     )]
-    pub sol_vault: SystemAccount<'info>,
+    pub escrow_account: Account<'info, EscrowAccount>,
     #[account(mut)]
     pub recipient: SystemAccount<'info>,
     #[account(mut)]
     pub signer: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Title should be less than or equal to 128 chars")]
+    TitleTooLong,
 }
--- tests/main.ts
+++ tests/main.ts
@@ -2,38 +2,40 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Main } from "../target/types/main";
 import { Keypair } from "@solana/web3.js"
-import { assert } from "chai";
 
 describe("main", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
   const provider = anchor.getProvider()
 
   const program = anchor.workspace.main as Program<Main>;
-  const keypair = new Keypair();
-  const amount = 10000000;
+  const receiver = new Keypair();
+
+  const amount = 1000000000;
   const amount_bn = new anchor.BN(amount);
 
   it("Initialize", async () => {
     const tx = await program.methods
         .initializeEscrow(
-            amount_bn
+            {
+              title: "Parser",
+              lamports: amount_bn,
+              seller: provider.publicKey,
+              buyer: receiver.publicKey,
+            }
         ).accounts({
           signer: provider.publicKey,
-        }).rpc();
+        })
+        .rpc();
     console.log("Your transaction signature", tx);
   });
 
   it("Proceed", async () => {
     const tx = await program.methods
-        .confirmDelivery(
-            amount_bn
-        ).accounts({
+        .confirmDelivery()
+        .accounts({
           signer: provider.publicKey,
-          recipient: keypair.publicKey,
+          recipient: receiver.publicKey,
         }).rpc();
-
-    const account_balance = await provider.connection.getBalance(keypair.publicKey);
-    assert.equal(amount, account_balance);
 
     console.log("Your transaction signature", tx);
   });
