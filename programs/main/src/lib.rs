5630faaa3432f3280195c36b8a02cae8d838a530
xolra0d <sn24008@edu.lu.lv>
2025-08-02 04:16:58 +0300

    basic create/release sol

--- programs/main/src/lib.rs
+++ programs/main/src/lib.rs
@@ -1,16 +1,93 @@
+use std::convert::AsRef;
 use anchor_lang::prelude::*;
+use anchor_lang::system_program::{transfer, Transfer};
 
-declare_id!("3vmh9KsfkgmTorEqbNeGpeWaK7ZUHWkdEH55BauXuzot");
+declare_id!("A6kupDWJAzffm1J3cKVNRBLL3zuNT7cXFFgeMxDPZ3pc");
 
 #[program]
 pub mod main {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_escrow(ctx: Context<InitializeEscrow>, amount: u64) -> Result<()> {
+        let sol_vault = &mut ctx.accounts.sol_vault;
+        let signer = &mut ctx.accounts.signer;
+        let system_program = &ctx.accounts.system_program;
+
+        let vault_balance_before = sol_vault.get_lamports();
+
+        transfer(
+           CpiContext::new(
+                system_program.to_account_info(),
+                Transfer {
+                    from: signer.to_account_info(),
+                    to: sol_vault.to_account_info(),
+                },
+           ),
+           amount,
+        )?;
+
+        let vault_balance_after = sol_vault.get_lamports();
+
+        require_eq!(vault_balance_before + amount, vault_balance_after);
+
+        Ok(())
+    }
+
+    pub fn confirm_delivery(ctx: Context<ConfirmDelivery>, amount: u64) -> Result<()> {
+        let sol_vault = &mut ctx.accounts.sol_vault;
+        let _signer = &mut ctx.accounts.signer;
+        let recipient  = &mut ctx.accounts.recipient;
+        let system_program = &ctx.accounts.system_program;
+
+        let vault_balance_before = sol_vault.get_lamports();
+
+        let bump = &[ctx.bumps.sol_vault];
+        let seeds = &[b"vault".as_ref(), bump];
+        let signer_seeds = &[&seeds[..]];
+
+        transfer(
+            CpiContext::new(
+                system_program.to_account_info(),
+                Transfer {
+                    from: sol_vault.to_account_info(),
+                    to: recipient.to_account_info(),
+                },
+            ).with_signer(signer_seeds),
+            amount,
+        )?;
+
+        let vault_balance_after = sol_vault.get_lamports();
+
+        require_eq!(vault_balance_before - amount, vault_balance_after);
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeEscrow<'info> {
+    #[account(
+        mut,
+        seeds=[b"vault".as_ref()],
+        bump,
+    )]
+    pub sol_vault: SystemAccount<'info>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct ConfirmDelivery<'info> {
+    #[account(
+        mut,
+        seeds=[b"vault".as_ref()],
+        bump
+    )]
+    pub sol_vault: SystemAccount<'info>,
+    #[account(mut)]
+    pub recipient: SystemAccount<'info>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
--- tests/main.ts
+++ tests/main.ts
@@ -1,16 +1,40 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Main } from "../target/types/main";
+import { Keypair } from "@solana/web3.js"
+import { assert } from "chai";
 
 describe("main", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.getProvider()
 
   const program = anchor.workspace.main as Program<Main>;
+  const keypair = new Keypair();
+  const amount = 10000000;
+  const amount_bn = new anchor.BN(amount);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("Initialize", async () => {
+    const tx = await program.methods
+        .initializeEscrow(
+            amount_bn
+        ).accounts({
+          signer: provider.publicKey,
+        }).rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Proceed", async () => {
+    const tx = await program.methods
+        .confirmDelivery(
+            amount_bn
+        ).accounts({
+          signer: provider.publicKey,
+          recipient: keypair.publicKey,
+        }).rpc();
+
+    const account_balance = await provider.connection.getBalance(keypair.publicKey);
+    assert.equal(amount, account_balance);
+
     console.log("Your transaction signature", tx);
   });
 });
