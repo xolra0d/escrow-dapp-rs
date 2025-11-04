ed30f8bf9d6b9863ac3d8a89239b064fe862bbef
xolra0d <sn24008@edu.lu.lv>
2025-11-04 18:39:27 +0200

    polish, allow multiple escrows for the same buyer and seller

--- programs/main/src/lib.rs
+++ programs/main/src/lib.rs
@@ -7,14 +7,16 @@ declare_id!("A6kupDWJAzffm1J3cKVNRBLL3zuNT7cXFFgeMxDPZ3pc");
 pub mod main {
     use super::*;
 
-    pub fn create_escrow(ctx: Context<CreateEscrow>, deal_info: DealInfo) -> Result<()> {
+    pub fn create_escrow(ctx: Context<CreateEscrow>, escrow_id: u32, deal_info: DealInfo) -> Result<()> {
         require!(
             deal_info.title.len() <= EscrowAccount::MAX_TITLE_LENGTH,
-            ErrorCode::TitleTooLong
+            Error::TitleTooLong
         );
+        require!(deal_info.lamports > 0, Error::InvalidLamports);
 
         let escrow_account = &mut ctx.accounts.escrow_account;
 
+        escrow_account.id = escrow_id;
         escrow_account.title = deal_info.title;
         escrow_account.lamports = deal_info.lamports;
         escrow_account.buyer = ctx.accounts.buyer.key();
@@ -35,9 +37,10 @@ pub mod main {
         Ok(())
     }
 
-    pub fn describe_escrow(ctx: Context<DescribeEscrow>) -> Result<()> {
+    pub fn describe_escrow(ctx: Context<DescribeEscrow>, escrow_id: u32) -> Result<()> {
         let escrow_account = &ctx.accounts.escrow_account;
 
+        msg!("ID: {}", escrow_account.id);
         msg!("Title: {}", escrow_account.title);
         msg!("Seller: {}", escrow_account.seller);
         msg!("Buyer: {}", escrow_account.buyer);
@@ -46,9 +49,9 @@ pub mod main {
         Ok(())
     }
 
-    pub fn confirm_escrow(ctx: Context<ConfirmEscrow>) -> Result<()> {
+    pub fn confirm_escrow(ctx: Context<ConfirmEscrow>, escrow_id: u32) -> Result<()> {
         let escrow_account = &mut ctx.accounts.escrow_account;
-        let seller  = &mut ctx.accounts.seller;
+        let seller = &mut ctx.accounts.seller;
 
         escrow_account.sub_lamports(escrow_account.lamports)?;
         seller.add_lamports(escrow_account.lamports)?;
@@ -56,13 +59,12 @@ pub mod main {
         Ok(())
     }
 
-    pub fn cancel_escrow(ctx: Context<CancelEscrow>) -> Result<()> {
+    pub fn cancel_escrow(ctx: Context<CancelEscrow>, escrow_id: u32) -> Result<()> {
         let escrow_account = &mut ctx.accounts.escrow_account;
-        let buyer  = &mut ctx.accounts.buyer;
+        let buyer = &mut ctx.accounts.buyer;
 
         escrow_account.sub_lamports(escrow_account.lamports)?;
         buyer.add_lamports(escrow_account.lamports)?;
-
 
         Ok(())
     }
@@ -77,6 +79,7 @@ pub struct DealInfo {
 
 #[account]
 pub struct EscrowAccount {
+    pub id: u32,
     pub title: String,
     pub lamports: u64,
     pub seller: Pubkey,
@@ -88,6 +91,7 @@ impl EscrowAccount {
     pub const MAX_TITLE_LENGTH: usize = 128;
 
     pub const MAX_SPACE: usize = 8 + // discriminator: u8, 8
+        4 + // escrow id: u32q
         4 + Self::MAX_TITLE_LENGTH + // title String
         8 + // lamports: u64
         32 + // buyer: pubkey
@@ -96,11 +100,12 @@ impl EscrowAccount {
 }
 
 #[derive(Accounts)]
+#[instruction(escrow_id: u32)]
 pub struct CreateEscrow<'info> {
     #[account(
         init,
         payer = buyer,
-        seeds = [b"sol_vault".as_ref(), seller.key().as_ref(), buyer.key().as_ref()],
+        seeds = [b"sol_vault".as_ref(), seller.key().as_ref(), buyer.key().as_ref(), escrow_id.to_le_bytes().as_ref()],
         bump,
         space = EscrowAccount::MAX_SPACE,
     )]
@@ -113,9 +118,10 @@ pub struct CreateEscrow<'info> {
 }
 
 #[derive(Accounts)]
+#[instruction(escrow_id: u32)]
 pub struct DescribeEscrow<'info> {
     #[account(
-        seeds = [b"sol_vault".as_ref(), seller.key().as_ref(), buyer.key().as_ref()],
+        seeds = [b"sol_vault".as_ref(), seller.key().as_ref(), buyer.key().as_ref(), escrow_id.to_le_bytes().as_ref()],
         bump = escrow_account.bump,
     )]
     pub escrow_account: Account<'info, EscrowAccount>,
@@ -126,13 +132,14 @@ pub struct DescribeEscrow<'info> {
 }
 
 #[derive(Accounts)]
+#[instruction(escrow_id: u32)]
 pub struct ConfirmEscrow<'info> {
     #[account(
         mut,
         close = buyer,
-        has_one = buyer @ ErrorCode::Unauthorised,
-        has_one = seller @ ErrorCode::WrongSeller,
-        seeds=[b"sol_vault".as_ref(), seller.key().as_ref(), buyer.key().as_ref()],
+        has_one = buyer @ Error::Unauthorised,
+        has_one = seller @ Error::WrongSeller,
+        seeds=[b"sol_vault".as_ref(), seller.key().as_ref(), buyer.key().as_ref(), escrow_id.to_le_bytes().as_ref()],
         bump = escrow_account.bump,
     )]
     pub escrow_account: Account<'info, EscrowAccount>,
@@ -145,19 +152,19 @@ pub struct ConfirmEscrow<'info> {
 }
 
 #[derive(Accounts)]
+#[instruction(escrow_id: u32)]
 pub struct CancelEscrow<'info> {
     #[account(
         mut,
         close = buyer,
-        has_one = buyer @ ErrorCode::Unauthorised,
-        has_one = seller @ ErrorCode::WrongSeller,
-        seeds=[b"sol_vault".as_ref(), seller.key().as_ref(), buyer.key().as_ref()],
+        has_one = buyer @ Error::Unauthorised,
+        has_one = seller @ Error::WrongSeller,
+        seeds=[b"sol_vault".as_ref(), seller.key().as_ref(), buyer.key().as_ref(), escrow_id.to_le_bytes().as_ref()],
         bump = escrow_account.bump,
-        constraint = signer.key() == escrow_account.buyer || signer.key() == escrow_account.seller @ ErrorCode::Unauthorised,
+        constraint = (signer.key() == escrow_account.buyer || signer.key() == escrow_account.seller) @ Error::Unauthorised,
     )]
     pub escrow_account: Account<'info, EscrowAccount>,
 
-    // #[account(mut)]
     pub signer: Signer<'info>,
     /// CHECK: we only need pubkey
     #[account(mut)]
@@ -168,9 +175,11 @@ pub struct CancelEscrow<'info> {
 }
 
 #[error_code]
-pub enum ErrorCode {
+pub enum Error {
     #[msg("Title should be less than or equal to 128 chars")]
     TitleTooLong,
+    #[msg("Lamports amount must be greater than zero")]
+    InvalidLamports,
     #[msg("Seller account should match account provided in deal_info!")]
     WrongSeller,
     #[msg("Buyer account should match account provided in deal_info!")]
--- tests/main.ts
+++ tests/main.ts
@@ -4,57 +4,57 @@ import { Main } from "../target/types/main";
 import { Keypair } from "@solana/web3.js"
 
 describe("main", () => {
-  anchor.setProvider(anchor.AnchorProvider.env());
-  const provider = anchor.getProvider()
+    anchor.setProvider(anchor.AnchorProvider.env());
+    const provider = anchor.getProvider()
 
-  const program = anchor.workspace.main as Program<Main>;
-  const receiver = new Keypair();
+    const program = anchor.workspace.main as Program<Main>;
+    const receiver = new Keypair();
 
-  const amount = 1000000000;
-  const amount_bn = new anchor.BN(amount);
-
-  it("Initialize", async () => {
-    const tx = await program.methods
-        .createEscrow(
-            {
-              title: "Parser",
-              lamports: amount_bn,
-              seller: receiver.publicKey,
-            }
-        ).accounts({
-          buyer: provider.publicKey,
-        seller: receiver.publicKey,
-        })
-        .rpc();
-    console.log("Your transaction signature", tx);
-  });
-
-  it("Describe", async () => {
-      const tx = await program.methods
-          .describeEscrow()
-          .accounts({
-              buyer: provider.publicKey,
-              seller: receiver.publicKey,
-          })
-          .rpc()
-      console.log("Your transaction signature", tx);
-  });
-
-  it("Confirm", async () => {
-    const tx = await program.methods
-        .confirmEscrow()
-        .accounts({
-          buyer: provider.publicKey,
-          seller: receiver.publicKey,
-        })
-        .rpc();
-
-    console.log("Your transaction signature", tx);
-  });
+    const amount = 1_000_000_000;
+    const amount_bn = new anchor.BN(amount);
 
     it("Initialize", async () => {
         const tx = await program.methods
-            .createEscrow(
+            .createEscrow(0,
+                {
+                    title: "Parser",
+                    lamports: amount_bn,
+                    seller: receiver.publicKey,
+                }
+            ).accounts({
+                buyer: provider.publicKey,
+                seller: receiver.publicKey,
+            })
+            .rpc();
+        console.log("Your transaction signature", tx);
+    });
+
+    it("Describe", async () => {
+        const tx = await program.methods
+            .describeEscrow(0)
+            .accounts({
+                buyer: provider.publicKey,
+                seller: receiver.publicKey,
+            })
+            .rpc()
+        console.log("Your transaction signature", tx);
+    });
+
+    it("Confirm", async () => {
+        const tx = await program.methods
+            .confirmEscrow(0)
+            .accounts({
+                buyer: provider.publicKey,
+                seller: receiver.publicKey,
+            })
+            .rpc();
+
+        console.log("Your transaction signature", tx);
+    });
+
+    it("Initialize", async () => {
+        const tx = await program.methods
+            .createEscrow(0,
                 {
                     title: "Validator",
                     lamports: amount_bn,
@@ -70,7 +70,7 @@ describe("main", () => {
 
     it("Describe", async () => {
         const tx = await program.methods
-            .describeEscrow()
+            .describeEscrow(0)
             .accounts({
                 buyer: provider.publicKey,
                 seller: receiver.publicKey,
@@ -81,7 +81,7 @@ describe("main", () => {
 
     it("Cancel", async () => {
         const tx = await program.methods
-            .cancelEscrow()
+            .cancelEscrow(0)
             .accounts({
                 signer: provider.publicKey,
                 buyer: provider.publicKey,
@@ -89,5 +89,51 @@ describe("main", () => {
             })
             .rpc()
         console.log("Your transaction signature", tx);
+    });
+
+    it("Initialize error: Same ID", async () => {
+        const tx1 = await program.methods
+            .createEscrow(0,
+                {
+                    title: "Parser",
+                    lamports: amount_bn,
+                    seller: receiver.publicKey,
+                }
+            ).accounts({
+                buyer: provider.publicKey,
+                seller: receiver.publicKey,
+            })
+            .rpc();
+
+
+        try {
+            const _ = await program.methods
+                .createEscrow(0,
+                    {
+                        title: "Validator",
+                        lamports: amount_bn,
+                        seller: receiver.publicKey,
+                    }
+                ).accounts({
+                    buyer: provider.publicKey,
+                    seller: receiver.publicKey,
+                })
+                .rpc();
+        } catch (error) {
+            if (error.message.includes("already in use")) {
+                console.log("Failed as expected.")
+            } else {
+                console.log("Unexpected error: ", error.message)
+            }
+        }
+
+        const cancel_escrow = await program.methods
+            .cancelEscrow(0,
+            ).accounts({
+                buyer: provider.publicKey,
+                seller: receiver.publicKey,
+            })
+            .rpc();
+        console.log("Your cancel transaction signature", cancel_escrow);
     });
 });
